15332db2eb62033ab70f6b744ee6816f34ce4073
Christopher Valarida <chris.valarida@adhocteam.us>
2021-07-09 14:56:39 -0700

    Load web components
    Not adding to package.json yet because I'm just symlinking to the local
    directory. For some reason, `npm link` and `yarn link` aren't working
    like I'd expect.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { defineCustomElements } from "web-components/loader";
+import "web-components/dist/component-library/component-library.css";
+
+defineCustomElements();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
