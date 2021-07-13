087d9807e2bf6fbdefb5a6ded96d9d68cffcef79
Christopher Valarida <chris.valarida@adhocteam.us>
2021-07-13 12:02:41 -0700

    Disable server-side rendering

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,20 @@ import "web-components/dist/component-library/component-library.css";
 
 defineCustomElements();
 
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  );
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SafeHydrate>
+      <Component {...pageProps} />
+    </SafeHydrate>
+  );
 }
 
 export default MyApp;
