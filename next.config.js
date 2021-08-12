52f6f71792e0fe3bcb280484615ff26b17c23b1c
Christopher Valarida <chris.valarida@adhocteam.us>
2021-08-12 16:10:45 -0700

    Resolve react and formik dependencies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
   target: "serverless",
@@ -9,5 +11,20 @@ module.exports = {
         destination: "/",
       },
     ];
+  },
+  // Override the resolve.alias to include react and formik because Formulate
+  // lists them as devDependencies as well, and when we use npm / yarn link to
+  // link to the local version of formulate, we want to resolve to only one
+  // version of these dependencies or things break.
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        react: path.resolve("./node_modules/react"),
+        formik: path.resolve("./node_modules/formik"),
+      },
+    };
+    return config;
   },
 };
